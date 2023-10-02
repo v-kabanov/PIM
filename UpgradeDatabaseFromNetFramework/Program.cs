0d4b97afd072ad9d87d5d4dde99862dc30a825d1
Vasily Kabanov <Vasily.Kabanov@gmail.com>
2023-10-03 10:13:49 +1100

    leftovers

---
+++ UpgradeDatabaseFromNetFramework/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using LiteDB;
+
+namespace UpgradeDatabaseFromNetFramework
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var db = new LiteDatabase($"Filename=c:\\temp\\pim\\Pim.db; Upgrade=true; Password=;");
+                Console.WriteLine("Version: {0}", db.GetCollection("Note")?.Count());
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e);
+            }
+            Console.ReadLine();
+        }
+    }
+}
